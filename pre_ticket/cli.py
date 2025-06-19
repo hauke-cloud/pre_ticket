0d09028b8a0f84b248c9f36dfefe340a198991b4
Hauke Mettendorf <hauke@mettendorf.it>
2025-06-19 12:51:05 +0200

    Added more valid branch types

--- pre_ticket/cli.py
+++ pre_ticket/cli.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 
 from .tickets import add_ticket_number
+from .branch_types import add_branch_type
 
 
 def parse_args(argv=None):
@@ -12,9 +13,9 @@ def parse_args(argv=None):
     )
     parser.add_argument("filenames", nargs="+")
     parser.add_argument(
-        "--regex", default="(feature|bug|chore)/(?P<ticket>[0-9]+)-.*", type=str
+        "--regex", default="(?P<branch_type>(feature|bug|chore|fix|docs|refactor))/(?P<ticket>[0-9]+)-.*", type=str
     )
-    parser.add_argument("--format", default="{message}\\n\\nRef: #{ticket}", type=str)
+    parser.add_argument("--format", default="{type}: {ticket} {message}\\n\\nRef: #{ticket}", type=str)
     return parser.parse_args(argv)
 
 
@@ -26,6 +27,7 @@ def main(argv=None):
     )  # argparse escapes backslash by default
 
     add_ticket_number(args.filenames[0], args.regex, format_template)
+    add_branch_type(args.filenames[0], args.regex, format_template)
 
 
 if __name__ == "__main__":
