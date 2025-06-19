85a072048bc9b89a5e531f60abdfb29cf120958a
Hauke Mettendorf <hauke@mettendorf.it>
2025-06-19 15:33:59 +0200

    Re-work and added more fields

--- pre_ticket/cli.py
+++ pre_ticket/cli.py
@@ -3,8 +3,7 @@
 import argparse
 import sys
 
-from .tickets import add_ticket_number
-from .branch_types import add_branch_type
+from .fields import add_fields
 
 
 def parse_args(argv=None):
@@ -15,7 +14,7 @@ def parse_args(argv=None):
     parser.add_argument(
         "--regex", default="(?P<branch_type>(feature|bug|chore|fix|docs|refactor))/(?P<ticket>[0-9]+)-.*", type=str
     )
-    parser.add_argument("--format", default="{type}: {ticket} {message}\\n\\nRef: #{ticket}", type=str)
+    parser.add_argument("--format", default="{branch_type}: {ticket} {message}\\n\\nRef: #{ticket}", type=str)
     return parser.parse_args(argv)
 
 
@@ -26,8 +25,7 @@ def main(argv=None):
         "unicode_escape"
     )  # argparse escapes backslash by default
 
-    add_ticket_number(args.filenames[0], args.regex, format_template)
-    add_branch_type(args.filenames[0], args.regex, format_template)
+    add_fields(args.filenames[0], args.regex, format_template)
 
 
 if __name__ == "__main__":
--- pre_ticket/fields.py
+++ pre_ticket/fields.py
@@ -10,6 +10,24 @@ def get_current_branch():
     return call_command(["git", "rev-parse", "--abbrev-ref", "HEAD"])
 
 
+def retrieve_ticket(branch, regex):
+    matches = re.match(regex, branch)
+    if matches:
+        ticket_number = matches.group("ticket")
+        return ticket_number
+
+
+def is_ticket_in_message(contents, ticket):
+    for line in contents.splitlines():
+        stripped = line.strip().lower()
+
+        if stripped == "" or stripped.startswith("#"):
+            continue
+
+        if ticket.lower() in stripped:
+            return True
+
+
 def retrieve_branch_type(branch, regex):
     matches = re.match(regex, branch)
     if matches:
@@ -28,21 +46,25 @@ def is_branch_type_in_message(contents, branch_type):
             return True
 
 
-def add_branch_type(filename, regex, format_template):
+def add_fields(filename, regex, format_template):
     branch = get_current_branch()
     branch_type = retrieve_branch_type(branch, regex)
+    ticket_number = retrieve_ticket(branch, regex)
 
-    if branch_type:
+    if branch_type and ticket_number:
         with io.open(filename, "r+") as fd:
             contents = fd.read()
 
             if (
-                is_branch_type_in_message(contents, branch_type)
+                (
+                    is_branch_type_in_message(contents, branch_type)
+                    and is_ticket_in_message(contents, ticket_number)
+                )
                 or not contents[:contents.find("\n")]
             ):
                 return
 
-            branch_type_msg = format_template.format(message=contents, branch_type=branch_type)
+            msg = format_template.format(message=contents, branch_type=branch_type, ticket=ticket_number)
             fd.seek(0)
-            fd.write(six.text_type(branch_type_msg))
+            fd.write(six.text_type(msg))
             fd.truncate()
